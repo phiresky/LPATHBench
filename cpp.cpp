b7eea7bd874ca0f303969d075a09348948d82f03
phiresky <phiresky@users.noreply.github.com>
2014-12-20 16:50:00 +0100

    C++ implementation with more refs

--- cpp.cpp
+++ cpp.cpp
@@ -1,20 +1,21 @@
-#include <ctime>
 #include <vector>
-#include <bitset>
 #include <fstream>
 #include <iostream>
-#include <string>
 #include <chrono>
 
 using namespace std;
 using namespace std::chrono;
 
+struct node;
+
 struct route{
-  int dest, cost;
+  node& dest;
+  const int cost;
 };
 
 struct node {
   vector<route> neighbours;
+  bool visited = false;
 };
 
 vector<node> readPlaces(){
@@ -23,36 +24,30 @@ vector<node> readPlaces(){
   vector<node> nodes(numNodes);
   int node, neighbour, cost;
   while (text >> node >> neighbour >> cost){
-    nodes[node].neighbours.push_back(route{neighbour, cost});
+    nodes[node].neighbours.push_back(route{nodes[neighbour], cost});
   }
   return nodes;
 }
 
-int getLongestPath(const vector<node> &nodes, const int nodeID, vector<size_t>& visited){
-  visited[nodeID] = true;
+int getLongestPath(vector<node> &nodes, node &node){
+  node.visited = true;
   int max=0;
-  for(const route &neighbour: nodes[nodeID].neighbours){
-    if (visited[neighbour.dest] == false){
-      const int dist = neighbour.cost + getLongestPath(nodes, neighbour.dest, visited);
+  for(const route &neighbour: node.neighbours){
+    if (!neighbour.dest.visited){
+      const int dist = neighbour.cost + getLongestPath(nodes, neighbour.dest);
       if (dist > max){
         max = dist;
       }
     }
   }
-  visited[nodeID] = false;
+  node.visited = false;
   return max;
 }
 
-int getLongestPath(const vector<node> &nodes)
-{
-  vector<size_t> visited(nodes.size());
-  return getLongestPath(nodes, 0, visited);
-}
-
-int main(int argc, char** argv){
-  auto nodes = readPlaces();
+int main() {
+  vector<node> nodes = readPlaces();
   auto start = high_resolution_clock::now();
-  int len = getLongestPath(nodes);
+  int len = getLongestPath(nodes, nodes[0]);
   auto end = high_resolution_clock::now();
   auto duration = (int)(0.001 * duration_cast<microseconds>(end - start).count());
   cout << len << " LANGUAGE C++ " << duration << std::endl;
